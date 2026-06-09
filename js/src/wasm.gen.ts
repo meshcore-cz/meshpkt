b9955f05c79ed4f6d7f8c19dc2748eb610ae6f0f
tree <tree@tree.fail>
2026-06-09 23:14:14 +0200

    bump version

--- js/src/wasm.gen.ts
+++ js/src/wasm.gen.ts
@@ -1128,7 +1128,6 @@ export const OpMetas: OpMeta[] = [
           { value: 1, label: "1" },
           { value: 2, label: "2" },
           { value: 3, label: "3" },
-          { value: 4, label: "4" },
         ],
         showWhen: "",
         showValue: 0,
